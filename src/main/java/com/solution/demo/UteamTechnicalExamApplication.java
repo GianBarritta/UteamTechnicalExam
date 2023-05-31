fe046cccb32450b86ac2691c0671f0ab2ed0a1f2
Gian <gianbdeveloper@hotmail.com>
2023-05-31 18:05:51 -0300

    Initial commit

---
+++ src/main/java/com/solution/demo/UteamTechnicalExamApplication.java
@@ -0,0 +1,13 @@
+package com.solution.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteamTechnicalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UteamTechnicalExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solution/demo/UteamTechnicalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteamTechnicalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
